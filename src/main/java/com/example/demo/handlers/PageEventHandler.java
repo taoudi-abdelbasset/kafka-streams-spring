af19fcfc4a3058cbd23bd4b66af6076b0a5361e9
taoudi-abdelbasset <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-10-05 20:46:05 +0100

    add a Stream process to consum topic T2 and do some data processing it return nbr of visit per user

--- src/main/java/com/example/demo/handlers/PageEventHandler.java
+++ src/main/java/com/example/demo/handlers/PageEventHandler.java
@@ -1,12 +1,17 @@
 package com.example.demo.handlers;
 
 import com.example.demo.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -30,5 +35,16 @@ public class PageEventHandler {
                     10+new Random().nextInt(10000)
             );
         };
+    }
+
+    @Bean
+    public Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction(){
+        return (input)->
+            input.filter((k,v)->v.duration()>100)
+                    .map((k,v)->new KeyValue<>(v.name(),v.duration()))
+                    .groupByKey(Grouped.with(Serdes.String(),Serdes.Long()))
+                    .count()
+                    .toStream()
+        ;
     }
 }
