8211601336eeea66e22c9ac7f66d3210f222ddf6
taoudi-abdelbasset <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-10-05 20:18:02 +0100

    Add Supplier send a message PageEvent in topic T2 every 800ms

--- src/main/java/com/example/demo/handlers/PageEventHandler.java
+++ src/main/java/com/example/demo/handlers/PageEventHandler.java
@@ -4,7 +4,10 @@ import com.example.demo.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -14,6 +17,18 @@ public class PageEventHandler {
             System.out.println("**********************");
             System.out.println(input.date());
             System.out.println("**********************");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+new Random().nextInt(10000)
+            );
         };
     }
 }
